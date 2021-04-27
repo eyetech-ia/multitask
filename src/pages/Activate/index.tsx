d7cec7ec730596e65e28708b14a0ed12f0ddc315
Victor Mesquita <mesquitadev@gmail.com>
2021-04-26 22:18:13 -0300

    M - activate

--- src/pages/Activate/index.tsx
+++ src/pages/Activate/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Layout, notification } from 'antd';
 
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import api from '../../services/api';
 
@@ -22,6 +22,7 @@ export const Container = styled.div`
 
 const Activate: React.FC = () => {
   const [message, setMessages] = useState<Messages[]>([]);
+  const history = useHistory();
   function useQuery() {
     return new URLSearchParams(useLocation().search);
   }
@@ -30,19 +31,29 @@ const Activate: React.FC = () => {
     const token = query.get('token');
     api.post('/activate-user', {
       token
+    }).then(() => {
+      notification.success({
+        message: 'Sucesso!',
+        description: 'Email validado com sucesso!, aguarde, redirecionando para o login',
+      });
+      setTimeout(() => {
+        history.push('/');
+      }, 3000);
     }).catch((res) => {
       notification.error({
         message: 'Erro!',
         description:
           res.response.data.message,
       });
+      setTimeout(() => {
+        history.push('/');
+      }, 3000);
     });
   }
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Content style={{ margin: '0 16px' }}>
-
         <div
           className="site-layout-background"
           style={{
